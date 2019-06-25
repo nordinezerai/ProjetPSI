b38646cfe89070fd4df3be69eca3a58ea5c1c76c
nordinezerai <nordine.zerai@gmail.com>
2019-06-25 23:24:38 +0200

    maj de la classe HiberateUtils

--- src/utils/HibernateUtils.java
+++ src/utils/HibernateUtils.java
@@ -1,24 +1,41 @@
 package utils;
 
-import org.hibernate.HibernateException;
-import org.hibernate.Session;
 import org.hibernate.SessionFactory;
-import org.hibernate.cfg.Configuration;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 
 public class HibernateUtils {
-	private static final SessionFactory sessionFactory;
+	private static StandardServiceRegistry registry;
+	private static SessionFactory sessionFactory;
 
-	// Crée une unique instance de la SessionFactory à partir de hibernate.cfg.xml
-	static {
-		try {
-			sessionFactory = new Configuration().configure().buildSessionFactory();
-		} catch (HibernateException ex) {
-			throw new RuntimeException("Problème de configuration : " + ex.getMessage(), ex);
+	public static SessionFactory getSessionFactory() {
+		if (sessionFactory == null) {
+			try {
+				registry = new StandardServiceRegistryBuilder()
+						.configure()
+						.build();
+
+				MetadataSources sources = new MetadataSources(registry);
+
+				Metadata metadata = sources.getMetadataBuilder().build();
+
+				sessionFactory = metadata.getSessionFactoryBuilder().build();
+
+			} catch (Exception e) {
+				e.printStackTrace();
+				if (registry != null) {
+					StandardServiceRegistryBuilder.destroy(registry);
+				}
+			}
 		}
+		return sessionFactory;
 	}
 
-	// Renvoie une session Hibernate
-	public static Session getSession() throws HibernateException {
-		return sessionFactory.openSession();
+	public static void shutdown() {
+		if (registry != null) {
+			StandardServiceRegistryBuilder.destroy(registry);
+		}
 	}
 }
